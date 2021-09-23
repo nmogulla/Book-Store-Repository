96b92c2a97ff2dc5c7c9025e49480afcdb11b21d
navaneetha <nmogulla@unomaha.edu>
2021-09-23 00:04:43 -0500

    Fixing an issue for email sending

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -124,7 +124,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 debug = False
 
